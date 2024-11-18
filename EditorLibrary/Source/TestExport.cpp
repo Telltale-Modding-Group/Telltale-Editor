8e641d7b10cb0b218d228eb389cb688604e541a4
Lucas Saragosa <lucassaragosasmith@hotmail.com>
2024-11-18 23:18:42 +0000

    xcode project setup

---
+++ EditorLibrary/Source/TestExport.cpp
@@ -0,0 +1,8 @@
+//
+//  TestExport.cpp
+//  EditorLibrary
+//
+//  Created by Lucas Saragosa on 18/11/2024.
+//
+
+#include <stdio.h>
---
+++ EditorUI/Source/Main.cpp
@@ -0,0 +1,8 @@
+//
+//  Main.cpp
+//  EditorUI
+//
+//  Created by Lucas Saragosa on 18/11/2024.
+//
+
+#include <stdio.h>
