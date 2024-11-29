8e33a93b3ab749ed028e041aa2ae29106d6a019a
Asil Zogby <asilz@stud.ntnu.no>
2024-11-30 00:13:01 +0100

    Implement SetThreadName for unix

--- EditorLibrary/Source/Scheduler/Platform/Unix/Thread.cpp
+++ EditorLibrary/Source/Scheduler/Platform/Unix/Thread.cpp
@@ -1,15 +1,17 @@
 #include <Config.hpp>
 #include <unistd.h>
+#include <pthread.h>
 
 void ThreadSleep(U64 milliseconds)
 {
     (void)sleep(milliseconds / 1000);
 }
 
-void SetThreadName(const String &tName) // TODO: Implement, might need to seperate linux and mac
+void SetThreadName(const String &tName)
 {
+    pthread_setname_np(pthread_self(), tName.c_str());
 }
 
-void DebugBreak() // TODO: Implement
+void DebugBreak() // TODO: Implement, might need to seperate linux and mac
 {
 }
