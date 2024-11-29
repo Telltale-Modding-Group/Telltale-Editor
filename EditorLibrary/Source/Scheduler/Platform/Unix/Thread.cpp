70ab40f6e051fd6601cd43b96f74b1ee03dec6d9
Asil Zogby <asilz@stud.ntnu.no>
2024-11-30 00:16:02 +0100

    Implement DebugBreak for unix

--- EditorLibrary/Source/Scheduler/Platform/Unix/Thread.cpp
+++ EditorLibrary/Source/Scheduler/Platform/Unix/Thread.cpp
@@ -1,5 +1,6 @@
 #include <Config.hpp>
 #include <unistd.h>
+#include <signal.h>
 #include <pthread.h>
 
 void ThreadSleep(U64 milliseconds)
@@ -12,6 +13,7 @@ void SetThreadName(const String &tName)
     pthread_setname_np(pthread_self(), tName.c_str());
 }
 
-void DebugBreak() // TODO: Implement, might need to seperate linux and mac
+void DebugBreak()
 {
+    (void)raise(SIGINT);
 }
