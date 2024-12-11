43902fdc4e1dda448409ffc6711ab7549ff20aee
Asil Zogby <asilz@stud.ntnu.no>
2024-12-11 12:36:30 +0100

    Document functions

--- TelltaleEditor/Source/ToolLibrary/Include/Scheduler/Thread.hpp
+++ TelltaleEditor/Source/ToolLibrary/Include/Scheduler/Thread.hpp
@@ -2,6 +2,21 @@
 
 #include <Config.hpp>
 
+/**
+ * @brief Sleeps the current thread for @p milliseconds time
+ *
+ * @param milliseconds duration in milliseconds
+ */
 void ThreadSleep(U64 milliseconds);
+
+/**
+ * @brief Sets the name of the current thread to @p tName
+ *
+ * @param tName name
+ */
 void SetThreadName(const String &tName);
+
+/**
+ * @brief Sets a breakpoint
+ */
 void DebugBreakpoint();
