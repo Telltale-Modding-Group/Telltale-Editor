27b1c8564b3ec9c760bee7fa7d999d2604de2014
Ivan Panayotov <ivanivov.p@gmail.com>
2024-12-25 15:16:58 +0200

    Improve codebase
    - Format code using our clang-format rules
    - Change some documentation
    - Convert some macros to constexprs

--- TelltaleEditor/Source/ToolLibrary/Source/Meta/Meta.cpp
+++ TelltaleEditor/Source/ToolLibrary/Source/Meta/Meta.cpp
@@ -1,127 +1,138 @@
-#include <Meta/Meta.hpp>
 #include <Core/Context.hpp>
+#include <Meta/Meta.hpp>
 #include <Scripting/LuaManager.hpp>
 
 #include <map>
 
-static ToolContext* GlobalContext = nullptr;
+static ToolContext *GlobalContext = nullptr;
 
-ToolContext* CreateToolContext()
+ToolContext *CreateToolContext()
 {
 
-    if(GlobalContext == nullptr){
-        GlobalContext = (ToolContext*)TTE_ALLOC(sizeof(ToolContext), MEMORY_TAG_TOOL_CONTEXT); // alloc raw and construct, as its private.
+    if (GlobalContext == nullptr)
+    {
+        GlobalContext = (ToolContext *)TTE_ALLOC(sizeof(ToolContext), MEMORY_TAG_TOOL_CONTEXT); // alloc raw and construct, as its private.
         new (GlobalContext) ToolContext();
-    }else{
+    }
+    else
+    {
         TTE_ASSERT(false, "Trying to create more than one ToolContext. Only one instance is allowed per process.");
     }
-    
+
     return GlobalContext;
 }
 
-ToolContext* GetToolContext()
-{
-    return GlobalContext;
-}
+ToolContext *GetToolContext() { return GlobalContext; }
 
 void DestroyToolContext()
 {
-    
-    if(GlobalContext)
+
+    if (GlobalContext)
         TTE_FREE(GlobalContext);
-    
+
     GlobalContext = nullptr;
-    
 }
 
-namespace Meta {
-    
-    // Registered game
-    struct RegGame {
-        String Name, ID;
-        StreamVersion MetaVersion;
-    };
-    
-    static std::vector<RegGame> Games{};
-    static std::map<U32, Class> Classes{};
-    static U32 GameIndex{}; // Current game index
-    
-    namespace L { // Lua functions
-        
-        U32 luaMetaRegisterGame(LuaManager& man){
-            RegGame reg{};
-            ScriptManager::TableGet(man, "Name");
-            reg.Name = ScriptManager::PopString(man);
-            ScriptManager::TableGet(man, "ID");
-            reg.ID = ScriptManager::PopString(man);
-            ScriptManager::TableGet(man, "MetaVersion");
-            
-            String v = ScriptManager::PopString(man);
-            
-            reg.MetaVersion = v == "MSV6" ? MSV6 : v == "MSV5" ? MSV5 : v == "MSV4" ? MSV4 : v == "MCOM" ? MCOM : v == "MTRE" ? MTRE : v == "MBIN" ? MBIN : v == "MBES" ? MBES : (StreamVersion)-1;
-            
-            TTE_ASSERT(reg.MetaVersion != (StreamVersion)-1, "Invalid meta version");
-            
-            Games.push_back(std::move(reg));
-            return 0;
-        }
-        
-        // Generates new registerable meta library lua API
-        LuaFunctionCollection luaMeta() {
-            LuaFunctionCollection Col{};
-            Col.Functions.push_back({"MetaRegisterGame",&luaMetaRegisterGame});
-            return Col;
-        }
-        
-    }
-    
-    void InitGame()
-    {
-        TTE_ASSERT(GetToolContext(), "Tool context not created");
-        GameSnapshot snap = GetToolContext()->GetSnapshot();
-        
-        ScriptManager::GetGlobal(GetToolContext()->GetLibraryLVM(), "RegisterAll", true);
-        
-        if(GetToolContext()->GetLibraryLVM().Type(-1) != LuaType::FUNCTION){
-            GetToolContext()->GetLibraryLVM().Pop(1);
-            TTE_ASSERT(false,"Cannot switch to new snapshot. Classes registration script is invalid");
-            return;
-        }else{
-            GetToolContext()->GetLibraryLVM().PushLString(snap.ID);
-            GetToolContext()->GetLibraryLVM().PushLString(snap.Platform);
-            GetToolContext()->GetLibraryLVM().PushLString(snap.Vendor);
-            ScriptManager::Execute(GetToolContext()->GetLibraryLVM(), 3, 1);
-        }
-        
-        TTE_LOG("Meta fully initialised with snapshot of %s", snap.ID.c_str());
-    }
-    
-    void RelGame()
-    {
-        Classes.clear();
-    }
-    
-    void Initialise()
-    {
-        TTE_ASSERT(GetToolContext(), "Tool context not created");
-        
-        ScriptManager::RegisterCollection(GetToolContext()->GetLibraryLVM(), L::luaMeta()); // Register meta scripting API
-        
-        ScriptManager::RunText(GetToolContext()->GetLibraryLVM(),
-                               GetToolContext()->LoadLibraryStringResource("Games.lua")); // Initialise games
-        
-        String src = GetToolContext()->LoadLibraryStringResource("Classes.lua");
-        ScriptManager::RunText(GetToolContext()->GetLibraryLVM(),  src);
-        src.clear();
-        
-        TTE_LOG("Meta partially initialised with %d games", (U32)Games.size());
-    }
-    
-    void Shutdown()
-    {
-        Games.clear();
-        
-        TTE_LOG("Meta shutdown");
-    }
-    
+namespace Meta
+{
+
+// Registered game
+struct RegGame
+{
+    String Name, ID;
+    StreamVersion MetaVersion;
+};
+
+static std::vector<RegGame> Games{};
+static std::map<U32, Class> Classes{};
+static U32 GameIndex{}; // Current game index
+
+namespace L
+{ // Lua functions
+
+U32 LuaMetaRegisterGame(LuaManager &man)
+{
+    RegGame reg{};
+    ScriptManager::TableGet(man, "Name");
+    reg.Name = ScriptManager::PopString(man);
+    ScriptManager::TableGet(man, "ID");
+    reg.ID = ScriptManager::PopString(man);
+    ScriptManager::TableGet(man, "MetaVersion");
+
+    String v = ScriptManager::PopString(man);
+
+    reg.MetaVersion = v == "MSV6"   ? MSV6
+                      : v == "MSV5" ? MSV5
+                      : v == "MSV4" ? MSV4
+                      : v == "MCOM" ? MCOM
+                      : v == "MTRE" ? MTRE
+                      : v == "MBIN" ? MBIN
+                      : v == "MBES" ? MBES
+                                    : (StreamVersion)-1;
+
+    TTE_ASSERT(reg.MetaVersion != (StreamVersion)-1, "Invalid meta version");
+
+    Games.push_back(std::move(reg));
+    return 0;
 }
+
+// Generates new registerable meta library lua API
+LuaFunctionCollection luaMeta()
+{
+    LuaFunctionCollection Col{};
+    Col.Functions.push_back({"MetaRegisterGame", &LuaMetaRegisterGame});
+    return Col;
+}
+
+} // namespace L
+
+void InitGame()
+{
+    TTE_ASSERT(GetToolContext(), "Tool context not created");
+    GameSnapshot snap = GetToolContext()->GetSnapshot();
+
+    ScriptManager::GetGlobal(GetToolContext()->GetLibraryLVM(), "RegisterAll", true);
+
+    if (GetToolContext()->GetLibraryLVM().Type(-1) != LuaType::FUNCTION)
+    {
+        GetToolContext()->GetLibraryLVM().Pop(1);
+        TTE_ASSERT(false, "Cannot switch to new snapshot. Classes registration script is invalid");
+        return;
+    }
+    else
+    {
+        GetToolContext()->GetLibraryLVM().PushLString(snap.ID);
+        GetToolContext()->GetLibraryLVM().PushLString(snap.Platform);
+        GetToolContext()->GetLibraryLVM().PushLString(snap.Vendor);
+        ScriptManager::Execute(GetToolContext()->GetLibraryLVM(), 3, 1);
+    }
+
+    TTE_LOG("Meta fully initialised with snapshot of %s", snap.ID.c_str());
+}
+
+void RelGame() { Classes.clear(); }
+
+void Initialise()
+{
+    TTE_ASSERT(GetToolContext(), "Tool context not created");
+
+    ScriptManager::RegisterCollection(GetToolContext()->GetLibraryLVM(), L::luaMeta()); // Register meta scripting API
+
+    ScriptManager::RunText(GetToolContext()->GetLibraryLVM(),
+                           GetToolContext()->LoadLibraryStringResource("Games.lua")); // Initialise games
+
+    String src = GetToolContext()->LoadLibraryStringResource("Classes.lua");
+    ScriptManager::RunText(GetToolContext()->GetLibraryLVM(), src);
+    src.clear();
+
+    TTE_LOG("Meta partially initialised with %d games", (U32)Games.size());
+}
+
+void Shutdown()
+{
+    Games.clear();
+
+    TTE_LOG("Meta shutdown");
+}
+
+} // namespace Meta
--- TelltaleEditor/Source/ToolLibrary/Source/Scripting/Adapter/Lua514.cpp
+++ TelltaleEditor/Source/ToolLibrary/Source/Scripting/Adapter/Lua514.cpp
@@ -56,9 +56,9 @@ Bool LuaAdapter_514::RunChunk(U8 *Chunk, U32 Len, Bool, CString Name)
     return false;
 }
 
-Bool LuaAdapter_514::LoadChunk(const String& nm, const U8* buf, U32 sz, Bool)
+Bool LuaAdapter_514::LoadChunk(const String &nm, const U8 *buf, U32 sz, Bool)
 {
-    int error = luaL_loadbuffer(_State, (const char*)buf, (size_t)sz, nm.c_str());
+    int error = luaL_loadbuffer(_State, (const char *)buf, (size_t)sz, nm.c_str());
     if (error == LUA_ERRSYNTAX)
     {
         TTE_LOG("Running %s: syntax error(s) => %s", nm.c_str(), lua_tostring(_State, -1));
@@ -76,9 +76,7 @@ Bool LuaAdapter_514::LoadChunk(const String& nm, const U8* buf, U32 sz, Bool)
     return false;
 }
 
-I32 LuaAdapter_514::UpvalueIndex(I32 index){
-    return lua_upvalueindex(index);
-}
+I32 LuaAdapter_514::UpvalueIndex(I32 index) { return lua_upvalueindex(index); }
 
 void LuaAdapter_514::CallFunction(U32 Nargs, U32 Nresults)
 {
@@ -100,58 +98,50 @@ void LuaAdapter_514::CallFunction(U32 Nargs, U32 Nresults)
     lua_pop(_State, 1); // Pop the error message string
 }
 
-Bool LuaAdapter_514::CheckStack(U32 extra)
-{
-    return lua_checkstack(_State, (int)extra);
-}
+Bool LuaAdapter_514::CheckStack(U32 extra) { return lua_checkstack(_State, (int)extra); }
 
-//adapter for passing in lua manager instead
-static int L514_CFunction(lua_State* L)
+// adapter for passing in lua manager instead
+static int L514_CFunction(lua_State *L)
 {
     LuaCFunction cfn = (LuaCFunction)lua_touserdata(L, lua_upvalueindex(1));
-    LuaManager& man = *((LuaManager*)lua_touserdata(L,lua_upvalueindex(1)));
+    LuaManager &man = *((LuaManager *)lua_touserdata(L, lua_upvalueindex(1)));
     return (int)cfn(man);
 }
 
-void LuaAdapter_514::Push(LuaType type, void* pValue)
+void LuaAdapter_514::Push(LuaType type, void *pValue)
 {
-    if(type == LuaType::NIL)
+    if (type == LuaType::NIL)
         lua_pushnil(_State);
-    else if(type == LuaType::BOOL)
-        lua_pushboolean(_State, *(Bool*)pValue);
-    else if(type == LuaType::LIGHT_OPAQUE)
+    else if (type == LuaType::BOOL)
+        lua_pushboolean(_State, *(Bool *)pValue);
+    else if (type == LuaType::LIGHT_OPAQUE)
         lua_pushlightuserdata(_State, pValue);
-    else if(type == LuaType::NUMBER)
-        lua_pushnumber(_State, (LUA_NUMBER)(*(Float*)pValue));
-    else if(type == LuaType::STRING)
-        lua_pushlstring(_State, ((String*)pValue)->c_str(), (size_t)((String*)pValue)->length());
-    else if(type == LuaType::TABLE)
+    else if (type == LuaType::NUMBER)
+        lua_pushnumber(_State, (LUA_NUMBER)(*(Float *)pValue));
+    else if (type == LuaType::STRING)
+        lua_pushlstring(_State, ((String *)pValue)->c_str(), (size_t)((String *)pValue)->length());
+    else if (type == LuaType::TABLE)
         lua_newtable(_State);
-    else if(type == (LuaType)999)
-        lua_pushvalue(_State,LUA_GLOBALSINDEX);
-    else if(type == (LuaType)888)
-        lua_pushinteger(_State, (LUA_INTEGER)(*(I32*)pValue));
-    else if(type == LuaType::FUNCTION){
+    else if (type == (LuaType)999)
+        lua_pushvalue(_State, LUA_GLOBALSINDEX);
+    else if (type == (LuaType)888)
+        lua_pushinteger(_State, (LUA_INTEGER)(*(I32 *)pValue));
+    else if (type == LuaType::FUNCTION)
+    {
         LuaCFunction fn = (LuaCFunction)pValue;
-        lua_pushlightuserdata(_State, (void*)&_Manager); // push lua manager instance upvalue
-        lua_pushlightuserdata(_State, (void*)fn); // push the function to be called's upvalue
+        lua_pushlightuserdata(_State, (void *)&_Manager); // push lua manager instance upvalue
+        lua_pushlightuserdata(_State, (void *)fn);        // push the function to be called's upvalue
         lua_pushcclosure(_State, &L514_CFunction, 2);
     }
 }
 
-void LuaAdapter_514::Pop(U32 N)
-{
-    lua_pop(_State, (int)N);
-}
+void LuaAdapter_514::Pop(U32 N) { lua_pop(_State, (int)N); }
 
-I32 LuaAdapter_514::GetTop()
-{
-    return (I32)lua_gettop(_State);
-}
+I32 LuaAdapter_514::GetTop() { return (I32)lua_gettop(_State); }
 
 void LuaAdapter_514::GetTable(I32 index, Bool bRaw)
 {
-    if(bRaw)
+    if (bRaw)
         lua_rawget(_State, (int)index);
     else
         lua_gettable(_State, (int)index);
@@ -159,31 +149,23 @@ void LuaAdapter_514::GetTable(I32 index, Bool bRaw)
 
 void LuaAdapter_514::SetTable(I32 index, Bool bRaw)
 {
-    if(bRaw)
+    if (bRaw)
         lua_rawset(_State, (int)index);
     else
         lua_settable(_State, (int)index);
 }
 
-void LuaAdapter_514::SetTableRaw(I32 index, I32 arrayIndex)
-{
-    lua_rawseti(_State, (int)index, (int)arrayIndex);
-}
+void LuaAdapter_514::SetTableRaw(I32 index, I32 arrayIndex) { lua_rawseti(_State, (int)index, (int)arrayIndex); }
 
-void LuaAdapter_514::GetTableRaw(I32 index, I32 arrayIndex)
-{
-    lua_rawgeti(_State, (int)index, (int)arrayIndex);
-}
+void LuaAdapter_514::GetTableRaw(I32 index, I32 arrayIndex) { lua_rawgeti(_State, (int)index, (int)arrayIndex); }
 
-I32 LuaAdapter_514::TableNext(I32 index)
-{
-    return (int)lua_next(_State, (int)index);
-}
+I32 LuaAdapter_514::TableNext(I32 index) { return (int)lua_next(_State, (int)index); }
 
 LuaType LuaAdapter_514::Type(I32 index)
 {
     int ltype = lua_type(_State, index);
-    switch(ltype){
+    switch (ltype)
+    {
         case LUA_TNONE:
         case LUA_TTHREAD:
         case LUA_TUSERDATA:
@@ -209,79 +191,37 @@ LuaType LuaAdapter_514::Type(I32 index)
 
 Bool LuaAdapter_514::Compare(I32 lhs, I32 rhs, LuaOp op)
 {
-    if(op == LuaOp::LT)
+    if (op == LuaOp::LT)
         return lua_lessthan(_State, lhs, rhs);
-    else if(op == LuaOp::LE)
+    else if (op == LuaOp::LE)
         return lua_lessthan(_State, lhs, rhs) || lua_equal(_State, lhs, rhs);
     return lua_equal(_State, lhs, rhs);
 }
 
-CString LuaAdapter_514::Typename(LuaType t)
-{
-    return lua_typename(_State, (int)t);
-}
+CString LuaAdapter_514::Typename(LuaType t) { return lua_typename(_State, (int)t); }
 
-Bool LuaAdapter_514::GetMetatable(I32 index)
-{
-    return (Bool)lua_getmetatable(_State,index);
-}
+Bool LuaAdapter_514::GetMetatable(I32 index) { return (Bool)lua_getmetatable(_State, index); }
 
-Bool LuaAdapter_514::SetMetatable(I32 index)
-{
-    return (Bool)lua_setmetatable(_State, index);
-}
+Bool LuaAdapter_514::SetMetatable(I32 index) { return (Bool)lua_setmetatable(_State, index); }
 
-void LuaAdapter_514::SetTop(I32 index)
-{
-    lua_settop(_State,(int)index);
-}
+void LuaAdapter_514::SetTop(I32 index) { lua_settop(_State, (int)index); }
 
-void LuaAdapter_514::PushCopy(I32 index)
-{
-    lua_pushvalue(_State,(int)index);
-}
+void LuaAdapter_514::PushCopy(I32 index) { lua_pushvalue(_State, (int)index); }
 
-void LuaAdapter_514::Remove(I32 index)
-{
-    lua_remove(_State, (int)index);
-}
+void LuaAdapter_514::Remove(I32 index) { lua_remove(_State, (int)index); }
 
-void LuaAdapter_514::Insert(I32 index)
-{
-    lua_insert(_State, (int)index);
-}
+void LuaAdapter_514::Insert(I32 index) { lua_insert(_State, (int)index); }
 
-void LuaAdapter_514::Replace(I32 index)
-{
-    lua_replace(_State,(int)index);
-}
+void LuaAdapter_514::Replace(I32 index) { lua_replace(_State, (int)index); }
 
-Bool LuaAdapter_514::ToBool(I32 index)
-{
-    return (Bool)lua_toboolean(_State,(int)index);
-}
+Bool LuaAdapter_514::ToBool(I32 index) { return (Bool)lua_toboolean(_State, (int)index); }
 
-Float LuaAdapter_514::ToFloat(I32 index)
-{
-    return (Float)lua_tonumber(_State, (int)index);
-}
+Float LuaAdapter_514::ToFloat(I32 index) { return (Float)lua_tonumber(_State, (int)index); }
 
-String LuaAdapter_514::ToString(I32 index)
-{
-    return String(lua_tolstring(_State, (int)index, 0));
-}
+String LuaAdapter_514::ToString(I32 index) { return String(lua_tolstring(_State, (int)index, 0)); }
 
-void* LuaAdapter_514::ToPointer(I32 index)
-{
-    return const_cast<void*>(lua_topointer(_State, (int)index));
-}
+void *LuaAdapter_514::ToPointer(I32 index) { return const_cast<void *>(lua_topointer(_State, (int)index)); }
 
-void LuaAdapter_514::Error()
-{
-    lua_error(_State);
-}
+void LuaAdapter_514::Error() { lua_error(_State); }
 
-I32 LuaAdapter_514::ToInteger(I32 index)
-{
-    return (I32)lua_tointeger(_State, (int)index);
-}
+I32 LuaAdapter_514::ToInteger(I32 index) { return (I32)lua_tointeger(_State, (int)index); }
